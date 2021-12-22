4549c87393f59f826954bbe73aa449f26d0b5dce
RY2291 <prgr2498@gmail.com>
2021-12-22 23:34:27 +0900

    [fin]4-4~5_セッションとクッキー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "/login", to: "sessions#new"
+  get 'sessions/new'
+  namespace :admin do
+    resources :users
+  end
   resources :tasks
   root to: "tasks#index"
 end
