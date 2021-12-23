fbb8d3107fe18b312ec2e2dc7d74d463066620fc
RY2291 <prgr2498@gmail.com>
2021-12-23 18:32:53 +0900

    [fin]5-9~11_system-共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,20 +2,30 @@ require "rails_helper"
 
 describe "タスク管理機能" do
   describe "一覧表示機能" do
+    let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@test.com") }
+    let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@test.com") }
+    
     before do
-      user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@test.com")
       FactoryBot.create(:task, name: "最初のタスク", user: user_a)
-    end
-    context "ユーザーAがログインしている時" do
-      before do
       visit login_path
-      fill_in "メールアドレス", with: "a@test.com"
-      fill_in "パスワード", with: "password"
+      fill_in "メールアドレス", with: login_user.email
+      fill_in "パスワード", with: login_user.password
       click_button "ログインする"
-      end
+    end
+
+    context "ユーザーAがログインしている時" do
+      let(:login_user) { user_a }
 
       it "ユーザーAが作成したタスクが表示される" do
-      expect(page).to have_content "最初のタスク"
+        expect(page).to have_content "最初のタスク"
+      end
+    end
+
+    context "ユーザーBがログインしている時" do
+      let(:login_user) { user_b }
+
+      it "ユーザーAが作成したタスクが表示されない" do
+        expect(page).to have_no_content "最初のタスク"
       end
     end
   end
