a45a65b78f12f919ae5c92ad2a794bd752e6de34
RY2291 <prgr2498@gmail.com>
2021-12-25 22:51:00 +0900

    [add]確認画面

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
-  resources :tasks
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
   root to: "tasks#index"
 end
